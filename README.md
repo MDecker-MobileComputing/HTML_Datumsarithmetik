48e346a838f569cd18ab74475c3d4f15f9683143
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-01-20 23:15:22 +0100

    First version

---
+++ README.md
@@ -0,0 +1,26 @@
+# Date Arithmetic
+
+The repository contains a simple web application for the calculation of the difference between two dates.
+Since the web application is stand-alone no server backend is required.
+The application is developed to be used on mobile devices (e.g. smartphones), so [jQuery Mobile](http://jquerymobile.com/)
+is used for the user interface.
+
+<br>
+
+----
+## Direct view
+
+The HTML file in this repository can be viewed directly at [this URL](https://mdecker-mobilecomputing.github.io/HTML_AndroidGlossar/index.html).
+
+<br>
+
+----
+## License
+
+See the [LICENSE file](LICENSE.md) for license rights and limitations (BSD 3-Clause License)
+for the files in this repository.
+
+The repository contains the files for [jQuery](https://jquery.com/) and [jQuery Mobile](http://jquerymobile.com/),
+which a distributed under the terms of the [MIT License](https://jquery.org/license/).
+Further, for working with dates the repository contains [Moment.js](https://momentjs.com/), which is also
+distributed under the terms of the [MIT License](https://tldrlegal.com/license/mit-license).
---
+++ docs/meinCode.js
@@ -0,0 +1,82 @@
+
+/* This file is licensed under the terms of the BSD 3-Clause License. */
+
+
+/* Datums-Muster für moment.js zum Formatieren und Parsen von Datums-Werten. */
+const DATUMS_MUSTER = "YYYY-MM-DD";
+
+/*
+ * Funktion liefert heutiges Datum in Format für Datums-Element zurück.
+ */
+function getDatumHeute() { "use strict";
+
+    return moment().format(DATUMS_MUSTER);
+}
+
+/*
+ * Funktion liefert morgiges Datum in Format für Datums-Element zurück.
+ */
+function getDatumMorgen() { "use strict";
+
+    return moment().add(1, "days").format(DATUMS_MUSTER);
+}
+
+
+/*
+ * Funktion berechnet die Differenz in Tagen zwischen zwei Datums-Werten und gibt diesen Wert zurück.
+ * Die Differenz in Tages ist nie ein negativer Wert, d.h. es wird ggf. das Minus-Zeichen abgeschnitten.
+ */
+function berechneDifferenz(datum1, datum2) { "use strict";
+    
+    let moment1 = moment(datum1, DATUMS_MUSTER);
+    let moment2 = moment(datum2, DATUMS_MUSTER);
+
+    let differenz = moment1.diff( moment2, "days" ); 
+    // ohne Angabe einer Einheit würde die Differenz in Millisekunden zurückgegeben.
+
+    return Math.abs( differenz );
+}
+
+
+/*
+ * Event-Handler für den "Berechnen"-Button.
+ */
+function onButtonBerechnen() { "use strict";
+
+    let datum1 = $("#datum-1").val();
+    let datum2 = $("#datum-2").val();
+    
+    let differenzTage = berechneDifferenz(datum1, datum2);
+
+    $("#ergebnis").text(`Differenz: ${differenzTage} Tage`);
+}
+
+
+/*
+ * Event-Handler für Button "Zurücksetzen"; wird aber auch zur Initialisierung
+ * der Applikation aufgerufen.
+ */
+function onButtonZuruecksetzen() { "use strict";
+
+    $("#datum-1").val( getDatumHeute()  );
+    $("#datum-2").val( getDatumMorgen() );
+
+    $("#ergebnis").text("");
+}
+
+
+/*
+ * Event-Handler für "Datei fertig geladen".
+ */
+function onSeiteGeladen() { "use strict";
+
+    $("#buttonBerechnen"    ).click( onButtonBerechnen     );
+    $("#buttonZuruecksetzen").click( onButtonZuruecksetzen );
+
+    onButtonZuruecksetzen();
+
+    console.log("Funktion onSeiteGeladen() abgearbeitet.");
+}
+
+
+$(document).on("pagecreate", onSeiteGeladen);
